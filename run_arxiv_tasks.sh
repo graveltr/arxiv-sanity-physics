468a0945ded6e1ee25be8eae6944bad2d0b6358c
Trevor Gravely <graveltr@umich.edu>
2024-08-19 21:21:59 -0500

    Pushing

--- run_arxiv_tasks.sh
+++ run_arxiv_tasks.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd /app
-
 export PYTHONPATH=/app
-
-python arxiv_daemon.py --num 2000
-python compute.py
+/usr/local/bin/python arxiv_daemon.py --num 2000
+/usr/local/bin/python compute.py
